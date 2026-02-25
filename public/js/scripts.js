bf300ccabb0f3cecf2fa232ea4bd2a31d1d1708a
BASILEIOS POULLAKKOS <t8230126@aueb.gr>
2026-02-25 21:33:11 +0200

    Add/Delete categories

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,36 @@
 document.getElementById('category-form').addEventListener('submit', async (e) =>{
     e.preventDefault();
-    const name = document.getElementById('category').value;
+    const categoryInput = document.getElementById('category')
+    const name = categoryInput.value;
     console.log('Category to add:', name);
 
-    await fetch('/categories', {
+    const response = await fetch('/categories', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name })
     });
+    if (response.ok) {
+        categoryInput.value = '';
+    } else {
+        alert('Error adding category');
+    }
 });
+
+document.querySelector('.delete-categories')
+    .addEventListener('click', deleteCategories);
+
+export async function deleteCategories() {
+    try {
+        const response =await fetch('/categories', {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            console.log('Categories deleted successfully');
+        } else {
+            alert('Error deleting categories');
+        }
+    } catch (err) {
+        console.error('Error deleting categories:', err);
+    }
+}
+
--- server.js
+++ server.js
@@ -8,7 +8,24 @@ app.use(express.static('public'));
 
 app.post('/categories', async (req, res) => {
     const name = req.body.name;
+    try {
+        await pool.query('INSERT INTO categories (name) VALUES ($1)', [name]);
+        res.status(201).send('Category added');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error adding category');
+    }
 })
+
+app.delete('/categories', async (req, res) => {
+    try {
+        await pool.query('DELETE FROM categories');
+        res.status(200).send('Categories deleted');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting categories');
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
